d26c530a21580cc5e09f905fef551a4106d3b763
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2022-09-10 18:36:09 +0300

    Create StoreItem component

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,27 @@
+import { Card } from "react-bootstrap";
+
+interface StoreItemProps {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+}
+
+export const StoreItem = ({ id, title, price, image }: StoreItemProps) => {
+  return (
+    <Card className="h-100">
+      <Card.Img
+        variant="top"
+        src={image}
+        height="200px"
+        style={{ objectFit: "cover" }}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{title}</span>
+          <span className="ms-2 text-muted">{price}</span>
+        </Card.Title>
+      </Card.Body>
+    </Card>
+  );
+};
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,18 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data.json";
+import { StoreItem } from "../components/StoreItem";
+
 export function Store() {
   return (
     <>
       <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} classname="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
     </>
   );
 }
