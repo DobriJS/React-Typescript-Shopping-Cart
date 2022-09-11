79b3af6b47f6167d021cd665d55e49b78109de82
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2022-09-11 22:21:48 +0300

    Add + and - buttons

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import { formatCurrency } from "../utils/formatCurrency";
 
 interface StoreItemProps {
@@ -9,6 +9,7 @@ interface StoreItemProps {
 }
 
 export const StoreItem = ({ id, title, price, image }: StoreItemProps) => {
+  const quantity = 0;
   return (
     <Card className="h-100">
       <Card.Img
@@ -19,9 +20,33 @@ export const StoreItem = ({ id, title, price, image }: StoreItemProps) => {
       />
       <Card.Body className="d-flex flex-column">
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-          <span className="fs-2">{title}</span>
+          <span className="fs-4">{title}</span>
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100">+ Add To Cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ gap: ".5rem" }}
+              >
+                <Button>-</Button>
+                <div>
+                  <span className="fs-3">{quantity}</span> in cart
+                </div>
+                <Button>+</Button>
+              </div>
+              <Button variant="danger" size="sm">
+                Remove
+              </Button>
+            </div>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
